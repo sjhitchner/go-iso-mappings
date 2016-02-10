8a92fc892582a21df2be14687439cc9839b9f71d
Stephen Hitchner <stephen@kiip.me>
2016-02-09 19:43:47 -0800

    adding mapping methods

---
+++ country/country.go
@@ -0,0 +1,62 @@
+// Package to help map ISO Country Codes
+//
+// ISO 3166-1
+// https://en.wikipedia.org/wiki/ISO_3166-1
+//
+// Defines standardized 2-digit, 3-digit, and numeric codes this package
+// provides a way to easily convert between them.
+//
+// All country data is contained in tab-delimited countries.txt.
+// To update with new country information edit this file with
+// the new information
+//
+// To compile this package you need to run:
+//  go generate
+//
+// This will generate all the necessary go maps that hold the mapping Data
+//
+package iso
+
+import (
+	"fmt"
+)
+
+//go:generate csv2map -d "\t" -in country.txt -k 2 -v 1 -m alpha2ToCountry -p iso -h
+// Map between Country Name and 2-digit alpha code
+func GetCountryNameFromISOAlpha2(alpha2 string) (string, error) {
+	country, ok := alpha3ToCountry[alpha2]
+	if !ok {
+		return "", fmt.Errorf("No country found for alpha3 '%s'", alpha2)
+	}
+	return country, nil
+}
+
+//go:generate csv2map -d "\t" -in country.txt -k 2 -v 1 -m alpha3ToCountry -p iso -h
+// Map between Country Name and 3-digit alpha code
+func GetCountryNameFromISOAlpha3(alpha3 string) (string, error) {
+	country, ok := alpha3ToCountry[alpha3]
+	if !ok {
+		return "", fmt.Errorf("No country found for alpha3 '%s'", alpha3)
+	}
+	return country, nil
+}
+
+//go:generate csv2map -d "\t" -in country.txt -k 2 -v 1 -m alpha3ToAlpha2 -p iso -h
+// Map between 3-digit alpha code and 2-digit alpha code
+func GetISOAlpha2FromAlpha3(alpha3 string) (string, error) {
+	alpha2, ok := alpha3ToAlpha2[alpha3]
+	if !ok {
+		return "", fmt.Errorf("No alpha2 code found for alpha2 '%s'", alpha3)
+	}
+	return alpha2, nil
+}
+
+//go:generate csv2map -d "\t" -in country.txt -k 2 -v 1 -m alpha2ToAlpha3 -p iso -h
+// Map between 3-digit alpha code and 2-digit alpha code
+func GetISOAlpha3FromAlpha2(alpha2 string) (string, error) {
+	alpha3, ok := alpha2ToAlpha3[alpha2]
+	if !ok {
+		return "", fmt.Errorf("No alpha3 code found for alpha2 '%s'", alpha2)
+	}
+	return alpha3, nil
+}
